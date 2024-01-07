0b5acdd7fc34e1982b8d4367c53d518430f0175b
Bharathrajangam <117607902+Bharathrajangam@users.noreply.github.com>
2024-01-06 19:27:48 -0500

    Add project files.

---
+++ Model.cs
@@ -0,0 +1,17 @@
+﻿namespace SeatingCharts
+{
+    // Seat.cs
+    public class Seat
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public bool IsOccupied { get; set; }
+    }
+
+    // SeatingLayout.cs
+    public class SeatingLayout
+    {
+        public List<Seat> Seats { get; set; } = new List<Seat>();
+    }
+
+}
